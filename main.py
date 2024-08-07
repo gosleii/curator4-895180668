8036bbd0b4115779d807565461a82b86c6c380af
gosleii <sleii@inbox.ru>
2024-08-07 20:33:51 +0300

    update

--- main.py
+++ main.py
@@ -13,39 +13,22 @@ def start(message):
 @bot.message_handler(commands=['anatomy'])
 def anatomy(message):
     bot.send_message(message.chat.id,
-                     '''Мастхев по анатомии - [канал Эдгара Кафарова](https://youtube.com/@kafarovanatom?si=im26L4ISdDw22r36)\n
-    Здесь
-    найдешь
-    объяснения
-    на
-    препаратах - [Андрей Стрелков](https: // youtube.com / @ user - rq4rn6gg5o?si = K7HiwhGWqxKNls8x)\n
-    Короткие
-    видео
-    для
-    повторения
-    материала - [Easy Anatomy](https: // youtube.com / @ easyanatomyapplication?si = zsgtOnSdC59BKTPZ)''')
+                     'Мастхев по анатомии - [канал Эдгара Кафарова](https://youtube.com/@kafarovanatom?si=im26L4ISdDw22r36)\n'
+                     'Здесь найдешь объяснения на препаратах - [Андрей Стрелков](https://youtube.com/@user-rq4rn6gg5o?si=K7HiwhGWqxKNls8x)\n'
+                     'Короткие видео для повторения материала - [Easy Anatomy](https: // youtube.com / @ easyanatomyapplication?si = zsgtOnSdC59BKTPZ)')
 
 
 @bot.message_handler(commands=['chemistry'])
 def chemistry(message):
     bot.send_message(message.chat.id,
-                     '''[Лекции, после который влюбишься в биохимию](https://youtube.com/@user-th9uf8ui9s?si=j4lzlfrwvgyK9tgV)\n
-                     [Биохимия
-    в
-    рисунках
-    и
-    схемах](https: // youtube.com / @ risuem_biohimiyu?si=H1u7nEhUtw__Xyqa)
-    ''')
+                     '[Лекции, после который влюбишься в биохимию](https://youtube.com/@user-th9uf8ui9s?si=j4lzlfrwvgyK9tgV)\n'
+                     '[Биохимия врисункахисхемах](https: // youtube.com / @ risuem_biohimiyu?si=H1u7nEhUtw__Xyqa)')
 
 @bot.message_handler(commands=['pharma'])
 def pharma(message):
     bot.send_message(message.chat.id,
-                     '''Разобраться в механизмах действия препаратов поможет [SciDrugs](https://youtube.com/@scidrugs?si=UqLB4sSaitSVyMt1)\n
-                     [Лучшие
-    лекции
-    по
-    фармакологии](https: // youtube.com / @ scidrugs?si=UqLB4sSaitSVyMt1)
-    ''')
+                     'Разобраться в механизмах действия препаратов поможет [SciDrugs](https://youtube.com/@scidrugs?si=UqLB4sSaitSVyMt1)\n'
+                     '[Лучшие лекции по фармакологии](https: // youtube.com / @ scidrugs?si=UqLB4sSaitSVyMt1)')
 
 @bot.message_handler(commands=['neurology'])
 def neurology(message):
